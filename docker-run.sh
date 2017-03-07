a374ea2b7798dad8409c4fe54067c0f9a86c3c1a
Matias Schertel <mschertel@gmail.com>
2017-03-06 21:23:20 -0300

    add line break

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -d -v $HOME/docker-data/sync/config:/syncthing/config -v $HOME/docker-data/sync/data:/syncthing/data -p 22000:22000  -p 21025:21025/udp -p 8080:8080 --name syncthing rpi-syncthing:arm
+docker run -d --name syncthing \
+           --restart=always \
+           -v $HOME/docker-data/sync/config:/syncthing/config \
+           -v $HOME/docker-data/sync/data:/syncthing/data \
+           -p 22000:22000 \
+           -p 21025:21025/udp \
+           -p 8080:8080 \
+           rpi-syncthing:arm
